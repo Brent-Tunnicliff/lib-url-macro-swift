998ac3c43bf8ca0d1e413ec1f8150675824f6cc8
Brent Tunnicliff <brent@tunnicliff.dev>
2026-08-12 20:31:57 +1000

    Fixed failing tests for some platforms.

--- Tests/MacroModuleTests/Helpers/Collection+safeTests.swift
+++ Tests/MacroModuleTests/Helpers/Collection+safeTests.swift
@@ -1,21 +1,28 @@
 // Copyright © 2026 Brent Tunnicliff <brent@tunnicliff.dev>
 
 import Testing
-@testable import MacroModule
-@testable import SwiftSyntax
+
+#if canImport(MacroModule)
+    @testable import MacroModule
+    private let canTestMacros = true
+#else
+    private let canTestMacros = true
+#endif
 
 @Suite("Collection+safeTests")
 struct CollectionSafeTests {
     let collection = 0..<9
 
-    @Test
+    @Test(.disabled(if: !canTestMacros))
     func subscriptSafeIndexInt() {
-        #expect(collection[safe: -1] == nil)
+        #if canImport(MacroModule)
+            #expect(collection[safe: -1] == nil)
 
-        for index in 0..<9 {
-            #expect(collection[safe: index] != nil)
-        }
+            for index in 0..<9 {
+                #expect(collection[safe: index] != nil)
+            }
 
-        #expect(collection[safe: 9] == nil)
+            #expect(collection[safe: 9] == nil)
+        #endif
     }
 }
